0ddf861d3a33c56cfc9f5743a1b22272419c401e
r315 <38652@alunos.isel.ipl.pt>
2018-07-17 00:50:24 +0100

    Add logic for starting and stop pwm

--- cmdpwm.cpp
+++ cmdpwm.cpp
@@ -13,10 +13,42 @@ void CmdPwm::start(void){
         PWM_Init(1000);
 	    PWM_Enable(1);
 	    PWM_Set(1, 50);
+        running = ON;
 }
 
 char CmdPwm::execute(void *ptr){
     char *p1, *p2;
-    start();
+    uint16_t arg;
+    
+   
+    p1 = strtok_s((char*)ptr, ' ', COMMAND_MAX_LINE - 10, &p2);
+
+    if( p1 == NULL){
+        help();
+        return CMD_OK;
+    }
+
+
+    arg = *((uint8_t*)ptr) << 8;
+    arg |= *((uint8_t*)(ptr+1));
+
+    switch(arg){
+        case '-' *256 + 's':
+            start();
+            break;
+
+         case '-' *256 + 'd':
+            arg = yatoi(p2);
+            if(arg >= 0 && arg < 101){
+                if (running == OFF){
+                    start();
+                }
+                PWM_Set(1, 50);
+            }
+            break;
+
+        default:
+            break;
+    }
     return CMD_OK;
 }
