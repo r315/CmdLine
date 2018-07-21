b9f95222c4450f6ea0ce378872eb6720eec22e7e
r315 <38652@alunos.isel.ipl.pt>
2018-07-21 19:39:12 +0100

    Add options co command pwm

--- cmdpwm.cpp
+++ cmdpwm.cpp
@@ -3,8 +3,10 @@
 
 void CmdPwm::help(void){
     vcom->printf("Usage: pwm [option] \n\n");  
-    vcom->printf("\t -s, start pwm\n");
+    vcom->printf("\t -r, run pwm (50%)\n");
     vcom->printf("\t -d duty, duty cycle 0-100 \n");
+    vcom->printf("\t -s, disable pwm\n");
+    vcom->printf("\t -t, enable pwm\n");
 }
 
 
@@ -20,30 +22,40 @@ char CmdPwm::execute(void *ptr){
     char *p1, *p2;
     uint16_t arg;
     
-   
+    // Check is has parameters
     p1 = strtok_s((char*)ptr, ' ', COMMAND_MAX_LINE - 10, &p2);
 
+    // if not show help
     if( p1 == NULL){
         help();
         return CMD_OK;
     }
 
+    // get first option
+    arg = ((uint8_t*)ptr)[0] << 8;
+    arg |= ((uint8_t*)(ptr))[1];
 
-    arg = *((uint8_t*)ptr) << 8;
-    arg |= *((uint8_t*)(ptr+1));
-
+    // test first option
     switch(arg){
         case '-' *256 + 's':
+             PWM_Disable(1);
+            break;
+
+        case '-' *256 + 't':
+             PWM_Enable(1);
+            break;
+
+        case '-' *256 + 'r':
             start();
             break;
 
-         case '-' *256 + 'd':
+        case '-' *256 + 'd':
             arg = yatoi(p2);
             if(arg >= 0 && arg < 101){
                 if (running == OFF){
                     start();
                 }
-                PWM_Set(1, 50);
+                PWM_Set(1, arg);
             }
             break;
 
