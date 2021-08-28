730ea6818eef6897a4525432957cebadc59e0d99
r315 <a38652@alunos.isel.pt>
2021-08-28 23:58:34 +0100

    WIP: Keyfob bit decoding

--- inc/cmdkeyfob.h
+++ inc/cmdkeyfob.h
@@ -16,13 +16,17 @@ typedef enum {BITSTREAM_RAW = 0, BITSTREAM_WIDTH, BITSTREAM_DECODE} bitstreamvie
 class CmdKeyFob : public ConsoleCommand{
     Console *console;
     uint16_t capture_data[CAP_BUF_SIZE];
+    uint32_t capture_count;
 
 public:
-    void init(void *params) { console = static_cast<Console*>(params);}
+    void init(void *params) { 
+        console = static_cast<Console*>(params);
+        capture_count = 0;
+    }
 
     char execute(void *ptr);
     void help(void);
-    void printBitstream(bitstreamview_e view, uint16_t *bitstream, uint32_t bittime, uint32_t streamlen);
+    void printBitstream(bitstreamview_e view, uint16_t *bitstream, int16_t bittime, uint8_t idle_state, uint32_t streamlen);
 
     CmdKeyFob () : ConsoleCommand("keyfob") { }    
 };
