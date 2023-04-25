771632f6682966945bce0f3eb4ff92429fd97e48
r315 <38652@alunos.isel.ipl.pt>
2023-04-25 15:24:34 +0100

    FIX: print dmips only on valid result

--- App/cmdbenchmark.cpp
+++ App/cmdbenchmark.cpp
@@ -8,6 +8,10 @@ void CmdBenchmark::help(void){
 
 char CmdBenchmark::execute(int argc, char **argv){
     float res = dhry(1000000) / 1757.0f;
-    console->print("%f DMIPS/MHz\n",  res / (SystemCoreClock / 1000000));
+
+    if(res){
+        console->print("%.2f DMIPS/MHz\n",  res / (SystemCoreClock / 1000000));
+    }
+
     return CMD_OK;
 }
