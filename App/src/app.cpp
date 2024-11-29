d157f13e19c36aa925bdb021bd04f49f61fa60b8
r315 <38652@alunos.isel.ipl.pt>
2024-11-29 20:16:50 +0000

    FIX: Build errors

--- App/src/app.cpp
+++ App/src/app.cpp
@@ -33,21 +33,21 @@
 #include "cmdsi5351.h"
 #include "cmdds1086.h"
 #include "cmdrgbled.h"
+#include "cmdsd.h"
+#include "stk500.h"
 
 #define WDT_TIMEOUT     3000
 
-#if defined (BOARD_BLUEBOARD)
-#include "cmdsd.h"
-#include "stk500.h"
-#endif
-
 static ConsoleCommand *app_commands[] = {
     new CmdHelp(),
-	new CmdEcho(),
     new CmdReset(),
-	//new CmdPwm(),
 	new CmdVersion(),
-    //new CmdMem(),
+#if defined (ENABLE_PWM)
+	new CmdPwm(),
+#endif
+#if defined (ENABLE_MEM)
+    new CmdMem(),
+#endif
 #if defined (ENABLE_TFT_DISPLAY)
 	new CmdTft(),
 #endif
@@ -69,36 +69,58 @@ static ConsoleCommand *app_commands[] = {
 #if defined (ENABLE_RGBLED)
     new CmdRgbled(),
 #endif
-
-#if defined (BOARD_BLUEBOARD)
-    new CmdSpi(),
-    //new CmdAvr(),
-    //new CmdSbus(),
-    new CmdSd(),
-	new CmdGpio(),
-	//new CmdMSensor(),
-	new CmdPlayer(),
-    
-#elif defined (BOARD_BLUEPILL)
-	new CmdRfinder(),
-	new CmdAdc(),
-	new CmdServo(),
-	new CmdSpiFlash(),
-	new CmdFlashRom(),
-	//new CmdEncoder(),
-#elif defined (BOARD_NUCLEO_L412KB)
-	new CmdBuz(),
-	new CmdKeyFob(),
-    new CmdLmcshd(),
+#if defined (ENABLE_PLAYER)
+    new CmdPlayer(),
 #endif
-
-#if defined(ENABLE_AVR_PROGRAMMING)
+#if defined (ENABLE_SDCARD)
+    new CmdSd(),
+#endif
+#if defined (ENABLE_AVR)
     new CmdAvr(),
+#endif
+#if defined (ENABLE_SPI)
+    new CmdSpi(),
+#endif
+#if defined (ENABLE_SBUS)
+    new CmdSbus(),
+#endif
+#if defined (ENABLE_GPIO)
+	new CmdGpio(),
+#endif
+#if defined (ENABLE_MSENSOR)
+	new CmdMSensor(),
+#endif
+#if defined (ENABLE_RFANDER)
+	new CmdRfinder(),
+#endif
+#if defined (ENABLE_ADC)
+	new CmdAdc(),
+#endif
+#if defined (ENABLE_SERVO)
+	new CmdServo(),
+#endif
+#if defined (ENABLE_FLASH)
+	new CmdSpiFlash(),
+#endif
+#if defined (ENABLE_FLASH_ROM)
+	new CmdFlashRom(),
+#endif
+#if defined (ENABLE_ENCODER)
+	new CmdEncoder(),
+#endif
+#if defined (ENABLE_BUZZER)
+	new CmdBuz(),
+#endif
+#if defined (ENABLE_KEYFOB)
+	new CmdKeyFob(),
+#endif
+#if defined (ENABLE_LMCSHD)
+    new CmdLmcshd(),
 #endif
 };
 
 extern "C" void App(void)
-{   
+{
     Console console;
     // stdout_t and serialops_t must be compatible for this to work
     stdout_t *userio = (stdout_t*)SERIAL_GetSerialOps(-1);
@@ -106,7 +128,7 @@ extern "C" void App(void)
     WDT_Init(WDT_TIMEOUT);
 
 #if defined (BOARD_BLUEBOARD)
-    console.init(userio, "Blueboard>");    
+    console.init(userio, "Blueboard>");
 #elif defined (BOARD_BLUEPILL)
     console.init(userio, "bluepill>");
 #elif defined (BOARD_NUCLEO_L412KB)
