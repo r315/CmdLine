a6191a2f3f2305ea546fce6b136c838917b786f7
r315 <38652@alunos.isel.ipl.pt>
2025-02-02 14:50:52 +0000

    UPD: ds1086 driver

--- App/libs/ds1086/ds1086.cpp
+++ App/libs/ds1086/ds1086.cpp
@@ -1,14 +1,60 @@
 #include "ds1086.h"
 #include "board.h"
+#include <string.h>
+
+
+typedef struct {
+    uint32_t min;
+    uint32_t max;
+}range_t;
+
+const range_t os_table [] = {
+    {30740000, 35860000},   // OS-6
+    {33300000, 38420000},   // OS-5
+    {35860000, 40980000},   // OS-4
+    {38420000, 43540000},   // OS-3
+    {40980000, 46100000},   // OS-2
+    {43540000, 48660000},   // OS-1
+    {46100000, 51220000},   // OS+0
+    {48660000, 53780000},   // OS+1
+    {51220000, 56340000},   // OS+2
+    {53780000, 58900000},   // OS+3
+    {56340000, 61460000},   // OS+4
+    {58900000, 64020000},   // OS+5
+    {61460000, 66580000},   // OS+6
+};
+
+static bool reg_read(i2cbus_t *i2c, uint8_t addr, uint8_t *dst, uint8_t len)
+{
+    if(I2C_Write(i2c, (uint8_t*)&addr, 1) != 1)
+        return false;
+
+    if(I2C_Read(i2c, dst, len) != len)
+        return false;
+
+    return true;
+}
+
+static bool reg_write(i2cbus_t *i2c, uint8_t addr, uint8_t *src, uint8_t len)
+{
+    len += 1;
+    uint8_t buf[len];
+    buf[0] = addr;
+
+    memcpy(buf + 1, src, len - 1);
+
+    return I2C_Write(i2c, buf, len) == len;
+}
 
 DS1086::DS1086()
 {
     m_i2c = NULL;
 }
 
-bool DS1086::init(i2cbus_t *i2c)
+int8_t DS1086::init(i2cbus_t *i2c)
 {
-    uint16_t reg_val;
+    uint8_t addr;
+
     m_i2c = i2c;
     m_i2c->addr = DS1086_DEVICE_ADDR << 1;
 
@@ -17,13 +63,198 @@ bool DS1086::init(i2cbus_t *i2c)
         DelayMs(100);
     }
 
-    if(read_reg(DS1086_RANGE, &reg_val) == true){
-        m_offset = (uint8_t)reg_val & 0x1f;
+    addr_read(&addr);
+
+    if(!(addr & DS1086_ADDR_WC)){
+        // Disable EEPROM auto write
+        addr_write (addr | DS1086_ADDR_WC);
+        DelayMs(100);
+    }
+
+    if(range_read(&m_os)){
+        return m_os;
+    }
+
+    return -1;
+}
+
+bool DS1086::addr_read(uint8_t *addr)
+{
+    return reg_read(m_i2c, DS1086_ADDR, addr, 1);
+}
+
+bool DS1086::addr_write(uint8_t addr)
+{
+    return reg_write(m_i2c, DS1086_ADDR, &addr, 1);
+}
+
+bool DS1086::range_read(uint8_t *os)
+{
+    uint8_t reg_val;
+
+    if(reg_read(m_i2c, DS1086_RANGE, &reg_val, 1)){
+        *os = reg_val & DS1086_RANGE_MASK;
         return true;
     }
 
     return false;
 }
+
+bool DS1086::offset_read(uint8_t *os)
+{
+    uint8_t reg_val;
+
+    if(reg_read(m_i2c, DS1086_OFFSET, &reg_val, 1)){
+        *os = reg_val & DS1086_OFFSET_MASK;
+        return true;
+    }
+
+    return false;
+}
+
+bool DS1086::offset_write(uint8_t os)
+{
+    return reg_write(m_i2c, DS1086_OFFSET, &os, 1);
+}
+
+bool DS1086::dac_read(uint16_t *value)
+{
+    uint8_t buf[2];
+
+    if(reg_read(m_i2c, DS1086_DAC, buf, 2)){
+        *value = (buf[0]<<2) | (buf[1] >> 6);
+        return true;
+    }
+
+    return false;
+}
+
+bool DS1086::dac_write(uint16_t value)
+{
+    uint8_t buf[2];
+
+    value = (value << 6) & DS1086_DAC_MASK;
+
+    buf[0] = (value >> 8);
+    buf[1] = value;
+
+    return reg_write(m_i2c, DS1086_DAC, buf, 2);
+}
+
+bool DS1086::prescaller_read(uint16_t *value)
+{
+    uint8_t buf[2];
+
+    if(reg_read(m_i2c, DS1086_PRES, buf, 2)){
+        *value = (buf[0]<<8) | buf[1];
+        return true;
+    }
+
+    return false;
+}
+
+bool DS1086::prescaller_write(uint16_t mask, uint16_t value)
+{
+    uint8_t buf[2];
+
+    if(!reg_read(m_i2c, DS1086_PRES, buf, 2)){
+        return false;
+    }
+
+    uint16_t reg_val = (buf[0] << 8) | buf[1];
+
+    reg_val = (reg_val & ~mask);
+
+    switch (mask){
+        case DS1086L_PRESCALLER_MASK:
+            reg_val |= value << 6;
+            break;
+        case DS1086L_JITTER_MASK:
+            reg_val |= value << 11;
+            break;
+        case DS1086L_OUT_CTRL_MASK:
+            reg_val |= value << 10;
+            break;
+        default:
+            return false;
+    }
+
+    buf[0] = reg_val >> 8;
+    buf[1] = reg_val;
+
+    return reg_write(m_i2c, DS1086_PRES, buf, 2);
+}
+
+/**
+ * @brief Find suitable table offset for a given
+ * master frequency.
+ *
+ * TODO: select offset for which the master oscillator is closer to
+ *       mid value
+ *
+ * @param [in] master_oscillator - Desired master oscillator frequency
+ * @param [out] min              - Minimum frequency of selected range
+ * @return int8_t                - Offset value [-6, 6]
+ */
+int8_t DS1086::findOffset(uint32_t master_oscillator, uint32_t *min)
+{
+    uint8_t table_size = sizeof(os_table) / sizeof(range_t);
+    uint8_t candidate = table_size/2;
+    int32_t delta = INT32_MAX;
+
+    // Go thtough all values on table and select the one
+    // with the smalest deviation of center frequency;
+    for(int i = 0; i < table_size; i++) {
+        if(master_oscillator >= os_table[i].min && master_oscillator <= os_table[i].max) {
+            int32_t center = os_table[i].min + ((os_table[i].max - os_table[i].min) / 2);
+            int32_t diff = center - master_oscillator;
+
+            diff = diff < 0 ? -diff : diff;
+
+            if(diff < delta){
+                delta = diff;
+                candidate = i;
+            }
+        }
+    }
+
+    *min = os_table[candidate].min;
+
+    return candidate - 6;
+}
+
+uint8_t DS1086::frequency_set(uint32_t freq)
+{
+    if(freq > DS1086L_FREQUENCY_MIN && freq < DS1086L_FREQUENCY_MAX){
+        uint32_t master_oscillator, dac;
+        uint8_t exp, offset;
+
+        // Find exponent
+        for(exp = 0; exp < 9; exp++){
+            master_oscillator = freq << exp;
+            if(master_oscillator > DS1086L_OSCILLATOR_MIN && master_oscillator < DS1086L_OSCILLATOR_MAX){
+                break;
+            }
+        }
+
+        uint32_t min;
+
+        // get new offset value and minimum frequency of new offset
+        offset = m_os + findOffset(master_oscillator, &min);
+
+        // DAC value is the difference between desired
+        // main oscillator frequency and range set by offset
+        dac = (master_oscillator - min) / DS1086L_DAC_STEP;
+
+        prescaller_write(DS1086L_PRESCALLER_MASK, exp); // Divide main oscillator to get desired frequency
+        dac_write(dac);                                 // Fine tune main oscillator frequency
+        offset_write(offset);                           // Set main oscillator frequency range
+        return true;
+    }
+
+    return false;
+}
+
 
 bool DS1086::read_reg(uint8_t reg, uint16_t *value)
 {
@@ -55,11 +286,11 @@ bool DS1086::write_reg(uint8_t reg, uint16_t value)
 
     switch(reg){
         case DS1086_PRES:
-        case DS1086_DAC:            
+        case DS1086_DAC:
             buf[0] = reg;
             buf[1] = value >> 8;
             buf[2] = value;
-            if(I2C_Write(m_i2c, (uint8_t*)&buf, 3) != 3) break;            
+            if(I2C_Write(m_i2c, (uint8_t*)&buf, 3) != 3) break;
             return true;
 
         case DS1086_OFFSET:
--- App/libs/ds1086/ds1086.h
+++ App/libs/ds1086/ds1086.h
@@ -8,6 +8,11 @@ extern "C" {
 #include <stdint.h>
 #include "i2c.h"
 
+/**
+ * @brief Basic driver to DS1086L
+ * https://www.analog.com/media/en/technical-documentation/data-sheets/DS1086L.pdf
+ */
+
 #define DS1086_DEVICE_ADDR  0x58
 #define DS1086_PRES         0x02
 #define DS1086_DAC          0x08
@@ -16,17 +21,44 @@ extern "C" {
 #define DS1086_RANGE        0x37
 #define DS1086_WRITE_EE     0x3F
 
+#define DS1086L_FREQUENCY_MIN   130000UL
+#define DS1086L_FREQUENCY_MAX   66600000UL
+#define DS1086L_OSCILLATOR_MIN  33300000UL
+#define DS1086L_OSCILLATOR_MAX  66600000UL
+#define DS1086L_PRESCALLER_MASK 0x03C0
+#define DS1086L_JITTER_MASK     0xF800
+#define DS1086L_OUT_CTRL_MASK   0x0400
+#define DS1086_RANGE_MASK       0x1F
+#define DS1086_OFFSET_MASK      0x1F
+#define DS1086_DAC_MASK         0xFFC0
+#define DS1086L_DAC_STEP        5000
+#define DS1086_ADDR_WC          (1 << 3)
+
 class DS1086
 {
 public:
     DS1086();
-    bool init(i2cbus_t*);
-    uint8_t getOffset(){ return m_offset; }
+    int8_t init(i2cbus_t*);
+    uint8_t frequency_set(uint32_t freq);
+    uint32_t min_freq_get(void){return DS1086L_FREQUENCY_MIN;}
+    uint32_t max_freq_get(void){return DS1086L_FREQUENCY_MAX;}
+
+    bool prescaller_read(uint16_t *value);
+    bool prescaller_write(uint16_t mask, uint16_t value);
+    bool dac_read(uint16_t *value);
+    bool dac_write(uint16_t value);
+    bool offset_read(uint8_t *of);
+    bool offset_write(uint8_t of);
+    bool range_read(uint8_t *os);
+    bool addr_read(uint8_t *ad);
+    bool addr_write(uint8_t ad);
+
     bool read_reg(uint8_t, uint16_t*);
 	bool write_reg(uint8_t, uint16_t);
 private:
     i2cbus_t *m_i2c;
-    uint8_t m_offset;
+    uint8_t m_os;
+    int8_t findOffset(uint32_t master_oscillator, uint32_t *min);
 };
 
 #ifdef __cplusplus
