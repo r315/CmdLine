a56f537b596af37b0cd3c3a21c571d7cd616b0ed
r315 <38652@alunos.isel.ipl.pt>
2026-02-19 19:28:00 +0000

    Integrated CoreMark benchmark

--- App/src/cmdbenchmark.cpp
+++ App/src/cmdbenchmark.cpp
@@ -40,6 +40,52 @@ static void updateDisplay(uint32_t *progress){
 }
 #endif
 
+#if defined(ENABLE_BENCHMARK_COREMARK)
+#include "CoreMark/coremark.h"
+
+#if VALIDATION_RUN
+	volatile ee_s32 seed1_volatile=0x3415;
+	volatile ee_s32 seed2_volatile=0x3415;
+	volatile ee_s32 seed3_volatile=0x66;
+#endif
+#if PERFORMANCE_RUN
+	volatile ee_s32 seed1_volatile=0x0;
+	volatile ee_s32 seed2_volatile=0x0;
+	volatile ee_s32 seed3_volatile=0x66;
+#endif
+#if PROFILE_RUN
+	volatile ee_s32 seed1_volatile=0x8;
+	volatile ee_s32 seed2_volatile=0x8;
+	volatile ee_s32 seed3_volatile=0x8;
+#endif
+	volatile ee_s32 seed4_volatile=ITERATIONS;
+	volatile ee_s32 seed5_volatile=0;
+
+static CORETIMETYPE start_time_val, stop_time_val;
+ee_u32 default_num_contexts=1;
+static Console *con;
+
+void start_time(void){start_time_val = GetTick(); }
+void stop_time(void){stop_time_val = GetTick(); }
+CORE_TICKS get_time(void) {return (CORE_TICKS)(stop_time_val - start_time_val);}
+secs_ret time_in_secs(CORE_TICKS ticks){ return (secs_ret) ((secs_ret)ticks) / (secs_ret)1000.0;}
+void portable_init(core_portable *p, int *argc, char *argv[]){(void)argc; (void)argv; p->portable_id=1;}
+void portable_fini(core_portable *p){p->portable_id=0;}
+
+int ee_printf(const char *format, ...)
+{
+    va_list args;
+    int len;
+    char buf[CONSOLE_WIDTH];
+
+	va_start(args, format);
+    len = strformater(buf, CONSOLE_WIDTH, format, args);
+	va_end(args);
+
+	return con->write(buf, len);
+}
+
+#endif
 static uint32_t piBenchmark(uint32_t iterations, float *calc_pi){
     uint32_t start, time;
     float x = 1.0f;
@@ -63,11 +109,18 @@ static uint32_t piBenchmark(uint32_t iterations, float *calc_pi){
     return time;
 }
 
+/**
+ * @brief Compile with -O3 for optimal performance
+ * @param
+ */
 void CmdBenchmark::help(void){
     console->println("Usage: benchmark <option>\n");
     console->print("\t pi   - PI benchmark\n");
     #if defined(ENABLE_BENCHMARK_DHRYSTONE)
     console->print("\t dhry - Dhrystone benchmark\n");
+    #endif
+    #if defined(ENABLE_BENCHMARK_COREMARK)
+    console->print("\t core - CoreMark benchmark\n");
     #endif
 }
 
@@ -77,6 +130,14 @@ char CmdBenchmark::execute(int argc, char **argv){
         help();
         return CMD_OK;
     }
+
+#if defined(ENABLE_BENCHMARK_COREMARK)
+    if(xstrcmp("core", (const char*)argv[1]) == 0){
+        con  = console;
+        coremark_main();
+        return CMD_OK;
+    }
+#endif
 
 #if defined(ENABLE_BENCHMARK_DHRYSTONE)
     if(xstrcmp("dhry", (const char*)argv[1]) == 0){
