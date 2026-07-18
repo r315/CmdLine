faf145a5c0c170e44354b18dff6560c723530b93
r315 <38652@alunos.isel.ipl.pt>
2026-07-18 01:29:18 +0100

    FIX: Build error

--- target/bluepill/Src/bp_main.c
+++ target/bluepill/Src/bp_main.c
@@ -234,10 +234,10 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
   */
 void Error_Handler(void)
 {
-  extern void defaultHandler(void);
+  extern void Default_Handler(void);
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
-  defaultHandler();
+  Default_Handler();
   /* USER CODE END Error_Handler_Debug */
 }
 
