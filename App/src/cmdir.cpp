14861c3fe8461ccac1765daf4e826bbb5b24bd66
r315 <38652@alunos.isel.ipl.pt>
2026-07-26 15:20:50 +0100

    IMP: rc6 ir protocol support

--- App/src/cmdir.cpp
+++ App/src/cmdir.cpp
@@ -1,32 +1,36 @@
 #include "cmdIr.h"
 #include "stimer.h"
 #include "rc6.h"
+#include <debug.h>
 
-static stimer_t rc6_timer;
-static uint16_t rc6_buf[44];
 static uint8_t rc6_tr;
-static uint8_t rc6_count;
+static stimer_t ir_timer;
+static uint16_t ir_buf[44];
+static uint8_t ir_repeat_count;
+static uint8_t ir_buf_len;
 
 
-static uint32_t rc6_repeat(stimer_t *timer)
+static uint32_t ir_repeat(stimer_t *timer)
 {
     (void)timer;
-    if(--rc6_count){
-        IR_Transmit(rc6_buf, 44);
+    if(--ir_repeat_count){
+        IR_Transmit(ir_buf, ir_buf_len);
     }
     return 0;
 }
 
-static void ir_eot_handler(void)
+static void ir_eot_handler(uint16_t unused)
 {
-    STIMER_Start(&rc6_timer);
+    (void)unused;
+    STIMER_Start(&ir_timer);
+}
 }
 
 void CmdIr::help(void)
 {
     console->println("Usage: ir <init|pulse|rc6>");
-    console->println("\tstart <pin number(hex)>");
-    console->println("\tpulse <duration>, Modulated pulse 270us to 5000us");
+    console->println("\0init <mode>,      0: TX 1: RX");
+    console->println("\tpulse <duration>, Modulated pulse > 400us");
     console->println("\trc6 <code>,       Transmit key code in RC6");
 }
 
@@ -38,7 +42,15 @@ char CmdIr::execute(int argc, char **argv)
     }
 
     if(!xstrcmp("init", argv[1])){
-        IR_Init();
+        uint32_t mode;
+        if(ha2u(argv[2], &mode)){
+            IR_Init((enum ir_mode)mode);
+
+            if(mode == IR_MODE_TX)
+                IR_SetCallback(ir_eot_handler);
+            else
+                IR_SetCallback(ir_eor_handler);
+        }
         return CMD_OK;
     }
 
@@ -49,19 +61,17 @@ char CmdIr::execute(int argc, char **argv)
             IR_Pulse(duration);
             return CMD_OK;
         }
-
     }
 
     if(!xstrcmp("rc6", argv[1])){
         uint32_t code;
         if(ha2u(argv[2], &code)){
-            uint8_t len = RC6_Frame(rc6_buf, rc6_tr++, 0, code);
-            rc6_count = 4;
-            rc6_timer.interval = 85;
-            rc6_timer.callback = rc6_repeat;
-            STIMER_Config(&rc6_timer);
-            IR_SetEotHandler(ir_eot_handler);
-            IR_Transmit(rc6_buf, len);
+            ir_buf_len = RC6_Frame(ir_buf, rc6_tr++, 0, code);
+            ir_repeat_count = 4;
+            ir_timer.interval = 85;
+            ir_timer.callback = ir_repeat;
+            STIMER_Config(&ir_timer);
+            IR_Transmit(ir_buf, ir_buf_len);
             return CMD_OK;
         }
     }
